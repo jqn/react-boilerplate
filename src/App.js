f600c79f9329841b38c5775d937b89e647b20ed0
jQN <jqn@jQNs-MacBook-Pro.local>
2020-02-16 19:57:48 +0100

    feature: added header left component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MainSection from "./containers/MainSection";
-import Header from "./components/Header";
+import Header from "./containers/Header";
 
 class App extends React.Component {
   render() {
