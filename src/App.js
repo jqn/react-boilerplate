ed09591887554683d3f9a2da257416e981c8c0d6
jQN <jqn@jQNs-MacBook-Pro.local>
2020-02-17 07:53:20 +0100

    feature: added react router and test routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainSection from "./containers/MainSection";
 import Header from "./containers/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <MainSection />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <MainSection />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import HeaderLeft from "../components/HeaderLeft";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <HeaderLeft />
-        <h1>Welcome to REACT!</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
