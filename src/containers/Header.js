c60c4fe582c97630cc8a1bfe84e5191f5b7abb78
jQN <jqn@jQNs-MacBook-Pro.local>
2020-02-17 11:33:15 +0100

    completed barebones homepage

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,27 +1,88 @@
-import React, { Component } from "react";
-import HeaderLeft from "../components/HeaderLeft";
-import { Link } from "react-router-dom";
+// import React, { Component } from "react";
+// import HeaderLeft from "../components/HeaderLeft";
+// import { Link } from "react-router-dom";
 
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
+// class Header extends Component {
+//   render() {
+//     return (
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="/">
+//               <h1>Reactor Labs</h1>
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to="/about">
+//               <p>About</p>
+//             </Link>
+//           </li>
+//           <li>
+//             <Link to="/users">
+//               <p>Users</p>
+//             </Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default Header;
+
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles(theme => ({
+  appBar: {},
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    fontFamily: '"Product Sans", serif',
+    fontWeight: "600",
+    color: "#000",
+  },
+  loginLink: {
+    fontFamily: '"Lora"',
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <AppBar
+      position="static"
+      className={classes.appBar}
+      elevation={0}
+      color="transparent"
+    >
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h4" className={classes.title}>
+          Reactor Labs
+        </Typography>
+        <Button color="inherit">
+          <Typography variant="subtitle1" className={classes.loginLink}>
+            Login
+          </Typography>
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
-
-export default Header;
--- src/containers/MainSection.js
+++ src/containers/MainSection.js
@@ -1,9 +1,20 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import "./styles/MainSection.css";
 
 const MainSection = () => (
   <section className="container">
-    <p>Simple base boilerplate</p>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+        />
+      </Container>
+    </React.Fragment>
   </section>
 );
 
