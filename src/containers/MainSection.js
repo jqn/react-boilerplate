974d750991576d8bd85a10db1c88ecce5fe53ef3
jQN <jqn@jQNs-MacBook-Pro.local>
2020-02-17 06:20:11 +0100

    docs: added example style

--- src/containers/MainSection.js
+++ src/containers/MainSection.js
@@ -1,8 +1,8 @@
 import React from "react";
-import style from "./styles/MainSection.css";
+import "./styles/MainSection.css";
 
 const MainSection = () => (
-  <section className={style.panelDefault}>
+  <section className="container">
     <p>Simple base boilerplate</p>
   </section>
 );
